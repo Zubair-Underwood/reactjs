4c0102ed7c8abf47125676703c0bc8cb73b96822
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-13 00:30:02 +0500

    learning

--- vite-project/src/components/LearnUsingENV.jsx
+++ vite-project/src/components/LearnUsingENV.jsx
@@ -3,10 +3,10 @@
 export const LearnUsingENV = () => {
   return (
     <>
-    <h1>How to use env in React + Vite</h1>
-    {import.meta.env.BASE_API_URL}
-    <br/>
-    {import.meta.env.API_KEY}
+      <h1>How to use env in React + Vite</h1>
+      {import.meta.env.VITE_BASE_API_URL}
+      <br />
+      {import.meta.env.VITE_API_KEY}
     </>
   )
 }
