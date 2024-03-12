85b806005dad3e1b07064f2f36f4aad9dd709e6e
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-03-13 00:16:17 +0500

    learning

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,7 @@ import { LearnConditionalRender } from "./components/LearnConditionalRender";
 import { LearnCustomHook } from "./components/LearnCustomHook";
 import { LearnExternalCss } from "./components/LearnExternalCss";
 import { LearnExternalCss2 } from "./components/LearnExternalCss2";
+import { LearnForm } from "./components/LearnForm";
 import { LearnInlineCss } from "./components/LearnInlineCss";
 import { LearnMap } from "./components/LearnMap";
 import { LearnUseOfImage } from "./components/LearnUseOfImage";
@@ -29,6 +30,7 @@ function App() {
       {/* <LearnCSSModule1/> */}
       {/* <LearnCSSModule2/> */}
       {/* <LearnUseOfImage /> */}
+      <LearnForm />
     </>
   )
 }
